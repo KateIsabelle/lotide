8004c49c6811c9c2c7aa5f726cb2ef7dbb39bb77
KateIsabelle <kate.davis887@gmail.com>
2021-03-02 20:58:33 +0000

    eqArrays function uses help of assertEqual function to check if two arrays are the same

---
+++ eqArrays.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return true;
+    //console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return false;
+    //console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+array1 = JSON.stringify(array1);
+array2 = JSON.stringify(array2);
+return assertEqual(array1, array2);
+}
+console.log(eqArrays([1,2,3], [1,2,3])); // returns true
+console.log(eqArrays([1,2,"3"], [1,2,3])); // returns false 

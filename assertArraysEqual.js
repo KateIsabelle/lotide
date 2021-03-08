e0ba579b4720c520fd5f57a09be6272778a72f9f
KateIsabelle <kate.davis887@gmail.com>
2021-03-08 19:11:59 +0000

    update eqArrays.js and assertArraysEqual.js to remove test functionality, and export function (to be tested in separate file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,9 +1,4 @@
-const eqArrays = function (array1, array2) {
-  array1 = JSON.stringify(array1);
-  array2 = JSON.stringify(array2);
-  return array1 === array2;
-  
-};
+const eqArrays = require('./eqArrays')
 
 const assertArraysEqual = function (array1, array2) {
   let assertion = eqArrays(array1, array2);
@@ -13,6 +8,5 @@ const assertArraysEqual = function (array1, array2) {
     console.log(`❌❌❌Assertion Failed: ${array1} !== ${array2}`);
   }
 }
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // pass
-assertArraysEqual([1, 2, "3"], [1, 2, 3]); // fail
-assertArraysEqual([1, 3, 2], [1, 2, 3]); // fail
+
+module.exports = assertArraysEqual;
--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -18,11 +10,9 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays;
+
+
 
 //code before refactoring:
 // const eqArrays = function(array1, array2) {

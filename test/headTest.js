1622eb7c50dacc02e727fc54cf28cc675a825e8f
KateIsabelle <kate.davis887@gmail.com>
2021-03-08 20:20:13 +0000

    update headTest.js to use mocha chai for testing and deleted our assertEqual function

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,14 @@
-const assertEqual = require('../assertEqual')
 const head = require('../head');
+const assert = require('chai').assert;
 
-assertEqual(head([5,6,7]), 5); // pass
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); //pass
-assertEqual(head([]), 5); // fail
-assertEqual(head([5,6,7]), 6); // fail
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+});
+
+
 

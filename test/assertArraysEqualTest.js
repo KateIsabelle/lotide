d9a6bcea625207d10f465553c0174cfc063fa93e
KateIsabelle <kate.davis887@gmail.com>
2021-03-08 19:09:18 +0000

    created test files for the test functions to check if they are working properly

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,8 @@
+const assertArraysEqual = require('../assertArraysEqual')
+
+console.log("should pass>")
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // pass
+console.log("should fail>")
+assertArraysEqual([1, 2, "3"], [1, 2, 3]); // fail
+console.log("should fail>")
+assertArraysEqual([1, 3, 2], [1, 2, 3]); // fail
